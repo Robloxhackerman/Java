a775c045b925e0817b44269b27ad98ba41bbcb3e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-10-14 11:13:25 -0300

    Create Main.java
    El mayor

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner tilin = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero:");
+        String v1 = tilin.nextLine();
+        int num1 = Integer.parseInt(v1);
+
+        System.out.println("Ingrese otro numero:");
+        String v2 = tilin.nextLine();
+        int num2 = Integer.parseInt(v2);
+        if (num2 < num1){
+            System.out.println("El primero es mayor");
+        } else if (num1 < num2) {
+            System.out.println("El segundo es mayo");
+        } else if (num1 == num2) {
+            System.out.println("son iguales");
+        }
+    }
+}
