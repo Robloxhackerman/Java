38a95cdd8702e747d316e37fa02a6c33379b6e45
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-10-31 01:09:40 -0300

    try1

---
+++ 06_Ciclos/Ciclo_while.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Method;
+
+public class Ciclo_while {
+    public static void main(String[] args) {
+        var count = 0;
+
+        while (count < 3) {
+            System.out.println("Contador :" + count );
+            count++;
+        }
+    }
+}
